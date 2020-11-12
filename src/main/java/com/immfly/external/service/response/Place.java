9aa687cd57a8ede3fa8c716cce098df4fa358ac9
Xavier Mas Leszkiewicz <xavier.mas.le@gmail.com>
2020-11-12 20:22:41 +0100

    Place Annotations

--- src/main/java/com/immfly/external/service/response/Place.java
+++ src/main/java/com/immfly/external/service/response/Place.java
@@ -1,5 +1,10 @@
 package com.immfly.external.service.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+@JsonIgnoreProperties
 public abstract class Place {
 
     private  String code;
